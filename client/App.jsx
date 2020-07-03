bd2cf34c5171e427938262a41f3879c774738372
Chelese Jensen <jensenchelese@gmail.com>
2020-07-02 19:00:55 -0700

    Update Presentation

--- client/App.jsx
+++ client/App.jsx
@@ -2,3 +2,28 @@
 
 
 //create react component that demonstrates vertical mode
+import React from 'react';
+import Carousel from 'react-elastic-carousel';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+      <Carousel itemsToScroll={2} itemsToShow={2}>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>4</p>
+        <p>5</p>
+        <p>6</p>
+        <p>7</p>
+        <p>8</p>
+      </Carousel>
+      </div>
+    );
+  }
+}
+export default App;
--- client/index.jsx
+++ client/index.jsx
@@ -1,1 +1,6 @@
 //render App js to html
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('carousel'));
